8dda83709f79236371be304d0be5e2c87f75a2bb
Nassif <n_nassif@icloud.com>
2026-05-01 08:28:50 +0200

    feat: add Vercel Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
 import { TopBar } from '@/components/topbar/TopBar'
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Nassif Nassif — Frontend Engineer',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <TopBar />
         {children}
+        <Analytics />
       </body>
     </html>
   )
