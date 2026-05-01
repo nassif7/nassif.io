3c6c21a7b9dedc408fa5efccd0aaec1cb4168b4f
Nassif <n_nassif@icloud.com>
2026-05-01 08:36:15 +0200

    feat: add Vercel Speed Insights integration to layout and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import '@/styles/globals.css'
 import { TopBar } from '@/components/topbar/TopBar'
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Nassif Nassif — Frontend Engineer',
@@ -19,6 +20,8 @@ export default function RootLayout({
         <TopBar />
         {children}
         <Analytics />
+        <SpeedInsights />
+
       </body>
     </html>
   )
